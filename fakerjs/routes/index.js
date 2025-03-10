a888af8c13e1e08555e26348f41510d771e72d01
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2025-03-10 12:41:02 +0000

    lezione 14 problema json

--- fakerjs/routes/index.js
+++ fakerjs/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var createError = require('http-errors');
+var faker = require('faker');
+const fs = require('fs');
 
-/* GET home page. */
+// Carica lista poeti salvata
+let poets = JSON.parse(fs.readFileSync('./poets.json', 'utf8'));
+
+/* GET home page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', poets });
+});
+
+/* GET poet detail page */
+router.get('/pages/:poet', function(req, res, next){
+  const poetID = parseInt(req.params.poet);
+  const poet = poets.find(p => p.id === poetID);
+  if (!poet) {
+    return next(createError(404, 'Poeta non trovato'));
+  }
+
+  res.render('poeta', { person: poet });
 });
 
 module.exports = router;
--- fakerjs/routes/users.js
+++ fakerjs/routes/users.js
@@ -4,20 +4,41 @@ var faker = require('faker');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(createFakePerson());
+    res.send(createFakePerson());
 });
+
+
+router.get('/generate', function(req, res, next) {
+  res.send(createJson());
+});
+
+function createJson() {
+  const fs = require('fs');
+  let poet = {
+  name: 'Mike',
+  age: 23,
+  gender: 'Male',
+  department: 'English',
+  car: 'Honda'
+  };
+  let data = JSON.stringify(poet);
+  fs.writeFileSync('poets.json', data);
+  return data
+}
 
 function createFakePerson()
   {
     let randomName = faker.name.findName(); // Rowan Nikolaus
     let randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
     let randomCard = faker.helpers.createCard(); // random contact card containing many properties
+    let randomImage = faker.image.avatar();
     let person = {
     name:randomName,
     email:randomEmail,
-    card: randomCard
+    card: randomCard,
+    image: randomImage
   }
     return person;
-}
+  }
 
 module.exports = router;
