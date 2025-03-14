b0914abd93c9c998a3e1f2b11fd58274698fd6f4
Riccardolotorto <113579508+Riccardolotorto@users.noreply.github.com>
2025-03-14 08:13:07 +0000

    faker completo

--- fakerjs/routes/index.js
+++ fakerjs/routes/index.js
@@ -1,26 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var createError = require('http-errors');
-var faker = require('faker');
-const fs = require('fs');
 
-// Carica lista poeti salvata
-let poets = JSON.parse(fs.readFileSync('./poets.json', 'utf8'));
-
-/* GET home page */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', poets });
-});
-
-/* GET poet detail page */
-router.get('/pages/:poet', function(req, res, next){
-  const poetID = parseInt(req.params.poet);
-  const poet = poets.find(p => p.id === poetID);
-  if (!poet) {
-    return next(createError(404, 'Poeta non trovato'));
-  }
-
-  res.render('poeta', { person: poet });
+  res.render('homehome', { title: 'Home' });
 });
 
 module.exports = router;
--- fakerjs/routes/users.js
+++ fakerjs/routes/users.js
@@ -1,44 +1,56 @@
 var express = require('express');
 var router = express.Router();
-var faker = require('faker');
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send(createFakePerson());
-});
-
-
-router.get('/generate', function(req, res, next) {
-  res.send(createJson());
-});
-
-function createJson() {
-  const fs = require('fs');
-  let poet = {
-  name: 'Mike',
-  age: 23,
-  gender: 'Male',
-  department: 'English',
-  car: 'Honda'
-  };
-  let data = JSON.stringify(poet);
-  fs.writeFileSync('poets.json', data);
-  return data
-}
+var faker = require('faker'); 
 
 function createFakePerson()
-  {
-    let randomName = faker.name.findName(); // Rowan Nikolaus
-    let randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-    let randomCard = faker.helpers.createCard(); // random contact card containing many properties
-    let randomImage = faker.image.avatar();
-    let person = {
+{
+  let randomName = faker.name.findName(); // Rowan Nikolaus
+  let randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+  let randomCard = faker.helpers.createCard(); // random contact card containing many properties
+  let person = {
     name:randomName,
     email:randomEmail,
-    card: randomCard,
-    image: randomImage
+    card: randomCard
   }
-    return person;
+  return person;
+}
+
+function fakePersonArray(arrayLenght) 
+{
+  let array = [];
+  for (let i = 0; i < arrayLenght; i++) {
+    array.push(createFakePerson());
+    console.log(array[i]);
   }
+  return array;
+}
+
+allpoets = fakePersonArray(10);
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+  res.render('index', {
+  title: 'Users',
+  poets: allpoets //Passa il vettore alla pagina index.pug
+  });
+});
+
+router.get('/:poet_username', (req, res) => { 
+  const poetUsername = req.params.poet_username;
+  for (i = 0; i < allpoets.length; i++) {
+    if (allpoets[i].card.username == poetUsername) {
+      var poet = allpoets[i];
+    } }
+  if (poet != null) {
+    res.render('poet', {
+      title: poet.name,
+      poet: poet
+    });
+  } else {
+    res.status(404).send('poet not found');
+  }
+
+});
+
 
 module.exports = router;
